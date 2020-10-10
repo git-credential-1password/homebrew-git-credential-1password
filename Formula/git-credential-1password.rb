642c9fdcbb0d849633b53f68216e1fb977c2301a
Deryck Lio <d@derycklio.com>
2020-10-10 13:30:45 +1100

    Set up the initial homebrew formula

---
+++ Formula/git-credential-1password.rb
@@ -0,0 +1,10 @@
+class GitCredential1Password < Formula
+  desc '1Password Git Credential Helper'
+  homepage 'https://github.com/git-credential-1password/git-credential-1password'
+  head 'https://github.com/git-credential-1password/git-credential-1password.git'
+  license ''
+
+  def install
+    bin.install 'git-credential-1password'
+  end
+end
