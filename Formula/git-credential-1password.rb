63ae9461a268114a0eec03bea845d9b810a20d6d
Deryck Lio <d@derycklio.com>
2020-10-10 13:53:32 +1100

    Add URL and license declaration

--- Formula/git-credential-1password.rb
+++ Formula/git-credential-1password.rb
@@ -2,7 +2,8 @@ class GitCredential1password < Formula
   desc '1Password Git Credential Helper'
   homepage 'https://github.com/git-credential-1password/git-credential-1password'
   head 'https://github.com/git-credential-1password/git-credential-1password.git'
-  license ''
+  url 'https://github.com/git-credential-1password/git-credential-1password/archive/v1.0.tar.gz'
+  license 'MIT'
 
   def install
     bin.install 'git-credential-1password'
