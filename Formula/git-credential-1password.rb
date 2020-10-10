caf8d054b9d1389d810c7068ad4b9b78ca9cae64
Deryck Lio <d@derycklio.com>
2020-10-10 13:33:40 +1100

    Fix forumla class name

--- Formula/git-credential-1password.rb
+++ Formula/git-credential-1password.rb
@@ -1,4 +1,4 @@
-class GitCredential1Password < Formula
+class GitCredential1password < Formula
   desc '1Password Git Credential Helper'
   homepage 'https://github.com/git-credential-1password/git-credential-1password'
   head 'https://github.com/git-credential-1password/git-credential-1password.git'
